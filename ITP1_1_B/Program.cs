c56f42a6c299a54abcffdfa5da5681b7c39f4a1d
JUNKI555 <junki555@gmail.com>
2021-08-27 17:26:49 +0900

    :pencil2: ITP1_1_B

---
+++ ITP1_1_B/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITP1_1_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow(x, 3));
+        }
+    }
+}
