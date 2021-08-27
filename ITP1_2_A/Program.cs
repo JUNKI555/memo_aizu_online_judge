c066f6398ac6cb00a3c9d9c8d01af13b00dfc56b
JUNKI555 <junki555@gmail.com>
2021-08-28 08:08:34 +0900

    :pencil2: ITP1_2_A

---
+++ ITP1_2_A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ITP1_2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+
+            if (a < b) {
+                Console.WriteLine("a < b");
+                return;
+            }
+
+            if (a > b) {
+                Console.WriteLine("a > b");
+                return;
+            }
+
+            Console.WriteLine("a == b");
+        }
+    }
+}
