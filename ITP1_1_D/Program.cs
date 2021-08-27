2290f63ed15f12edeb8d4126af99b14171d75d2f
JUNKI555 <junki555@gmail.com>
2021-08-28 07:59:49 +0900

    :pencil2: ITP1_1_D

---
+++ ITP1_1_D/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ITP1_1_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seconds = int.Parse(Console.ReadLine());
+            var timeSpan = new TimeSpan(0, 0, seconds);
+            Console.WriteLine(timeSpan.ToString(@"h\:m\:s"));
+        }
+    }
+}
