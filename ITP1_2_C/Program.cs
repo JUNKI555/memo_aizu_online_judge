28b5e379ed52c2c06fdeaceb7437fcee442a5dd2
JUNKI555 <junki555@gmail.com>
2021-08-28 08:24:32 +0900

    :pencil2: ITP1_2_C

---
+++ ITP1_2_C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITP1_2_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Split(' ');
+            var intList = new List<int>();
+            intList.Add(int.Parse(line[0]));
+            intList.Add(int.Parse(line[1]));
+            intList.Add(int.Parse(line[2]));
+            intList.Sort();
+
+            Console.WriteLine($"{intList[0]} {intList[1]} {intList[2]}");
+        }
+    }
+}
