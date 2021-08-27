a70dd976dded630340ae297c94485a6e215e65a4
JUNKI555 <junki555@gmail.com>
2021-08-28 07:47:05 +0900

    :pencil2: ITP1_1_C

---
+++ ITP1_1_C/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ITP1_1_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+            var area = a*b;
+            var perimeter = a+a+b+b;
+
+            Console.WriteLine($"{area} {perimeter}");
+        }
+    }
+}
