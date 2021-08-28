7c6e2faa4aa110f435eecc1fe44008a6af012b90
JUNKI555 <junki555@gmail.com>
2021-08-28 09:11:45 +0900

    :pencil2: ITP1_3_B

---
+++ ITP1_3_B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ITP1_3_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var i = 0;
+            var endFlag = true;
+            while (endFlag) {
+                i++;
+                var x = int.Parse(System.Console.ReadLine());
+                if (x == 0) {
+                    endFlag = false;
+                } else {
+                    Console.WriteLine($"Case {i}: {x}");
+                }
+            }
+
+            Console.Out.Flush();
+        }
+    }
+}
