b9585e0d70e354ee1c31e82523c54d5ba6cb66e1
JUNKI555 <junki555@gmail.com>
2021-08-28 13:51:06 +0900

    :pencil2: ITP1_6_A

---
+++ ITP1_6_A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITP1_6_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberCount = int.Parse(System.Console.ReadLine());
+
+            var line = System.Console.ReadLine().Split(' ');
+            var intList = new List<decimal>();
+            for (var i = 0; i < numberCount; i++) {
+                intList.Add(decimal.Parse(line[i]));
+            }
+
+            intList.Reverse();
+
+            var result = "";
+            for (var i = 0; i < numberCount; i++) {
+                if (i == 0) {
+                    result += intList[i].ToString();
+                    continue;
+                }
+
+                result += " " + intList[i].ToString();
+            }
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
