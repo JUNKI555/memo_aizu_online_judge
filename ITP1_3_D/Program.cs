cf69c5ecf8b6c5f46778cf715b4902a58776ca42
JUNKI555 <junki555@gmail.com>
2021-08-28 09:30:45 +0900

    :pencil2: ITP1_3_D

---
+++ ITP1_3_D/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ITP1_3_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+            var c = int.Parse(line[2]);
+
+            var divisorCout = 0;
+            for (var i = a; i <= b; i++) {
+                if ((c % i) == 0) {
+                    divisorCout++;
+                }
+            }
+
+            Console.WriteLine($"{divisorCout}");
+        }
+    }
+}
