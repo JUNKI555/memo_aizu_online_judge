e10285ad43461e512e52945bba373d3ae5dcc993
JUNKI555 <junki555@gmail.com>
2021-08-28 10:41:55 +0900

    :pencil2: ITP1_4_D

---
+++ ITP1_4_D/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITP1_4_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberCount = int.Parse(System.Console.ReadLine());
+
+            var line = System.Console.ReadLine().Split(' ');
+            var intList = new List<decimal>();
+            for (var i = 0; i < numberCount; i++) {
+                intList.Add(decimal.Parse(line[i]));
+            }
+
+            Console.WriteLine($"{intList.Min()} {intList.Max()} {intList.Sum()}");
+        }
+    }
+}
