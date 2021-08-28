bd8022c68aef314d1efec1887d1c2da5994f3c4a
JUNKI555 <junki555@gmail.com>
2021-08-28 09:38:20 +0900

    :pencil2: ITP1_4_A

---
+++ ITP1_4_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ITP1_4_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+
+            Console.WriteLine($"{a/b} {a%b} {a/(double)b:F8}");
+        }
+    }
+}
