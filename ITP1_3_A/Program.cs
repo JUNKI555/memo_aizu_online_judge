06ab5d97853de9ce6cefc0430c4f2524e690427d
JUNKI555 <junki555@gmail.com>
2021-08-28 09:01:25 +0900

    :pencil2: ITP1_3_A

---
+++ ITP1_3_A/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ITP1_3_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            for(var i = 0; i < 1000; i++) {
+                Console.WriteLine("Hello World");
+            }
+
+            Console.Out.Flush();
+        }
+    }
+}
