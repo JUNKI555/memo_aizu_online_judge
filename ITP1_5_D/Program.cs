384135cc8efd2dfc586d6977077ae08fd40a7562
JUNKI555 <junki555@gmail.com>
2021-08-28 13:12:18 +0900

    :pencil2: ITP1_5_D

---
+++ ITP1_5_D/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ITP1_5_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var n = int.Parse(System.Console.ReadLine());
+            var result = "";
+            var x = 0;
+            for (var i = 1; i <= n; i++) {
+                x = i;
+                if ((x % 3) == 0) {
+                    result += " " + i.ToString();
+                    continue;
+                }
+                
+                if ((x % 10) == 3) {
+                    result += " " + i.ToString();
+                    continue;
+                }
+
+                while(x != 0) {
+                    x = x / 10;
+                    if ((x % 10) == 3) {
+                        result += " " + i.ToString();
+                        x = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.Out.Flush();
+        }
+    }
+}
