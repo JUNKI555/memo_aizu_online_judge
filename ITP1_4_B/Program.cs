035dc9f6aa4509692f46b3f72e5f4292eaa79354
JUNKI555 <junki555@gmail.com>
2021-08-28 10:02:37 +0900

    :pencil2: ITP1_4_B

---
+++ ITP1_4_B/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ITP1_4_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse(Console.ReadLine());
+            var circleArea = r * r * Math.PI;
+            var circlePerimeter = 2 * r * Math.PI;
+
+            Console.WriteLine($"{circleArea:F6} {circlePerimeter:F6}");
+        }
+    }
+}
