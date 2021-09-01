14f769fe2831cd42c6a91e852ac98392c2aa0d64
JUNKI555 <junki555@gmail.com>
2021-09-01 17:21:21 +0900

    :pencil2: ITP1_7_C

---
+++ ITP1_7_C/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITP1_7_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var qLine = System.Console.ReadLine().Split(' ');
+            var r = int.Parse(qLine[0]);
+            var c = int.Parse(qLine[1]);
+            const string space = " ";
+
+            var colSumList = new int[c];
+            for(int j = 0; j < c; j++) {
+                colSumList[j] = 0;
+            }
+
+            for(int i = 0; i < r; i++) {
+                var row = new List<int>();
+                var line = System.Console.ReadLine().Split(' ');
+                for(int j = 0; j < c; j++) {
+                    var number = int.Parse(line[j]);
+                    row.Add(number);
+                    colSumList[j] += number;
+                }
+
+                var rowResult = String.Join(space, row);
+                rowResult += (space + row.Sum().ToString());
+                Console.WriteLine(rowResult);
+            }
+
+            var result = String.Join(space, colSumList);
+            result += (space + colSumList.Sum().ToString());
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+    }
+}
