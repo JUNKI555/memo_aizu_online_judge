e127b1c292462cec3293eda806550fb8b4588501
JUNKI555 <junki555@gmail.com>
2021-09-01 20:45:39 +0900

    :pencil2: ITP1_8_D

---
+++ ITP1_8_D/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ITP1_8_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = System.Console.ReadLine();
+            var p = System.Console.ReadLine();
+
+            var circleString = s + s;
+            if (circleString.Contains(p)) {
+                Console.WriteLine("Yes");
+            } else {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
