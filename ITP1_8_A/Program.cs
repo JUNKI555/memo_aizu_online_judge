ad0552595a0c9558ec292dcfc055c8b37b198cd3
JUNKI555 <junki555@gmail.com>
2021-09-01 19:49:24 +0900

    :pencil2: ITP1_8_A

---
+++ ITP1_8_A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ITP1_8_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = System.Console.ReadLine();
+            if (string.IsNullOrEmpty(s)) {
+                return;
+            }
+
+            var result = String.Empty;
+            foreach (var c in s)
+            {
+                if (Char.IsUpper(c)) {
+                    result += Char.ToLower(c);
+                } else {
+                    result += Char.ToUpper(c);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
