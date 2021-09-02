637855dc11fda142149b10d636e57b3ab24d937f
JUNKI555 <junki555@gmail.com>
2021-09-02 14:58:33 +0900

    :pencil2: ITP1_10_B

---
+++ ITP1_10_B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ITP1_10_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var line = System.Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+            var c = int.Parse(line[2]);
+
+            double radian = Math.PI * c / 180.0;
+            var sinC = Math.Sin(radian);
+
+            var h = b * sinC;
+            var s = (a * b / 2) * sinC;
+
+            var l = a + b + Math.Sqrt((a * a) + (b * b) - (2 * a * b * Math.Cos(radian)));
+
+            Console.WriteLine(s.ToString("F13"));
+            Console.WriteLine(l.ToString("F13"));
+            Console.WriteLine(h.ToString("F13"));
+            Console.Out.Flush();
+        }
+    }
+}
