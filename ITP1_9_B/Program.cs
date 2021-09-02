d00f8523f765b99381f5b5785eae7356815f0933
JUNKI555 <junki555@gmail.com>
2021-09-02 11:19:31 +0900

    :pencil2: ITP1_9_B

---
+++ ITP1_9_B/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITP1_9_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var continueFlag = true;
+            while (continueFlag) {
+                var text = System.Console.ReadLine();
+                if (text == "-") {
+                    continueFlag = false;
+                    continue;
+                }
+
+                var cardQueue = new Queue<string>();
+                foreach (var c in text)
+                {
+                    cardQueue.Enqueue(c.ToString());
+                }
+
+                var operationCount = int.Parse(System.Console.ReadLine());
+                for (int i = 0; i < operationCount; i++)
+                {
+                    var h = int.Parse(System.Console.ReadLine());
+                    for (int j = 0; j < h; j++)
+                    {
+                        var dequeueChar = cardQueue.Dequeue();
+                        cardQueue.Enqueue(dequeueChar);
+                    }
+                }
+
+                Console.WriteLine(String.Join("", cardQueue));
+            }
+
+            Console.Out.Flush();
+        }
+    }
+}
