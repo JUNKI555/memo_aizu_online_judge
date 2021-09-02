e561782919acada577b78603ca6b729586c983fa
JUNKI555 <junki555@gmail.com>
2021-09-02 11:33:12 +0900

    :pencil2: ITP1_9_C

---
+++ ITP1_9_C/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ITP1_9_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(System.Console.ReadLine());
+
+            const int winScore = 3;
+            const int drawScore = 1;
+            var scoreTaro = 0;
+            var scoreHanako = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var line = System.Console.ReadLine().Split(' ');
+                var taro = line[0];
+                var hanako = line[1];
+
+                var judge = taro.CompareTo(hanako);
+                if (judge == 0) {
+                    scoreTaro += drawScore;
+                    scoreHanako += drawScore;
+                } else if (judge < 0) {
+                    scoreHanako += winScore;
+                } else {
+                    scoreTaro += winScore;
+                }
+            }
+
+            Console.WriteLine($"{scoreTaro} {scoreHanako}");
+        }
+    }
+}
