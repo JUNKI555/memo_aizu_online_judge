0156f1c5a1682d6238df316cf8b891e91a190ac6
JUNKI555 <junki555@gmail.com>
2021-09-02 16:38:47 +0900

    :pencil2: ITP1_10_C

---
+++ ITP1_10_C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITP1_10_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var continueFlag = true;
+            while (continueFlag) {
+                var n = int.Parse(System.Console.ReadLine());
+                if (n == 0) {
+                    continueFlag = false;
+                    continue;
+                }
+
+                var numberList = new List<double>();
+                var line = System.Console.ReadLine().Split(' ');
+                for (int i = 0; i < n; i++)
+                {
+                    numberList.Add(double.Parse(line[i]));
+                }
+                Console.WriteLine(calcStdev(numberList).ToString("F13"));
+            }
+
+            Console.Out.Flush();
+        }
+
+        static double calcStdev(List<double> list) {
+            var average = list.Average();   // 平均
+            var sum2 = list.Select(x => x * x).Sum();   // 自乗和
+            var variance = (sum2 / list.Count) - (average * average);   // 分散 = 自乗和 / 要素数 - 平均値^2
+            return Math.Sqrt(variance);
+        }
+    }
+}
