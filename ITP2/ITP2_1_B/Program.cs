0cb2da8430feb8ec24ebaf3ca37f57b46a8fcba6
JUNKI555 <junki555@gmail.com>
2021-09-06 18:58:35 +0900

    :pencil2: ITP2_1_B

--- ITP2/ITP2_1_B/Program.cs
+++ ITP2/ITP2_1_B/Program.cs
@@ -57,3 +57,60 @@ namespace ITP2_1_B
         }
     }
 }
+
+
+// TLE : LinkedList
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// namespace ITP2_1_B
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+//             Console.SetOut(sw);
+
+//             var q = int.Parse(Console.ReadLine());
+
+//             var list = new LinkedList<int>();
+//             for (int i = 0; i < q; i++)
+//             {
+//                 var line = System.Console.ReadLine().Split(' ');
+//                 var command = line[0];
+//                 var dp = int.Parse(line[1]);
+
+//                 // push
+//                 if (command == "0") {
+//                     var x = int.Parse(line[2]);
+
+//                     if (dp == 0) {
+//                         list.AddFirst(x);
+//                     } else {
+//                         list.AddLast(x);
+//                     }
+
+//                     continue;
+//                 }
+
+//                 // pop
+//                 if (command == "2") {
+//                     if (dp == 0) {
+//                         list.RemoveFirst();
+//                     } else {
+//                         list.RemoveLast();
+//                     }
+
+//                     continue;
+//                 }
+
+//                 // randomAccess
+//                 Console.WriteLine(list.Skip(dp).First().ToString());
+//             }
+
+//             Console.Out.Flush();
+//         }
+//     }
+// }
