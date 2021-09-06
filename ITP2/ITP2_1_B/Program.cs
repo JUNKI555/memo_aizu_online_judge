b974089b495622835c5031a1413f8554ffb685cd
JUNKI555 <junki555@gmail.com>
2021-09-06 17:32:10 +0900

    :pencil2: ITP2_1_B

---
+++ ITP2/ITP2_1_B/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITP2_1_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            var q = int.Parse(Console.ReadLine());
+
+            var list = new int[q*2];
+            var head = q;
+            var ass = q+1;
+            for (int i = 0; i < q; i++)
+            {
+                var line = System.Console.ReadLine().Split(' ');
+                var command = line[0];
+                var dp = int.Parse(line[1]);
+
+                // push
+                if (command == "0") {
+                    var x = int.Parse(line[2]);
+
+                    if (dp == 0) {
+                        list[head] = x;
+                        head--;
+                    } else {
+                        list[ass] = x;
+                        ass++;
+                    }
+
+                    continue;
+                }
+
+                // pop
+                if (command == "2") {
+                    if (dp == 0) {
+                        head++;
+                    } else {
+                        ass--;
+                    }
+
+                    continue;
+                }
+
+                // randomAccess
+                Console.WriteLine(list[head+1+dp].ToString());
+                // Console.WriteLine($"answer: {list[head+1+dp]}");
+            }
+
+            Console.Out.Flush();
+        }
+    }
+}
